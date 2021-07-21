11d3d56d42d93169242edd0c5754d54f88a0edff
bodrius <bogdanpechenyk@gmail.com>
2021-07-21 10:54:53 +0300

    feat: implement material bottom nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import {QueryClient, QueryClientProvider} from 'react-query';
+import {NavigationContainer} from '@react-navigation/native';
+
+import {MainStackNavigator} from './navigators/MainStackNavigator';
 
 const App = () => {
-  return <></>;
+  // Create a client
+  const queryClient = new QueryClient();
+
+  // // Access the client
+  // const queryClient = useQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <MainStackNavigator />
+      </NavigationContainer>
+    </QueryClientProvider>
+  );
 };
 export default App;
